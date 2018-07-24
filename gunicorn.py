5fce8ec89db7a639d2c31d23993ff65498b1bcce
Robin Rheem <roborheems@gmail.com>
2018-07-24 23:07:08 +0900

    Fix gunicorn access and error log path

--- gunicorn.py
+++ gunicorn.py
@@ -148,9 +148,9 @@ tmp_upload_dir = None
 #       A string of "debug", "info", "warning", "error", "critical"
 #
 
-errorlog = 'logs/gunicorn_error_log.log'
+errorlog = './gunicorn_error.log'
 loglevel = 'info'
-accesslog = 'logs/gunicorn_access_log.log'
+accesslog = './gunicorn_access.log'
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 
 #

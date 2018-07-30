5d459d9d1b56832044dd195ceed5de241879f233
Robin Rheem <roborheems@gmail.com>
2018-07-30 21:46:31 +0900

    Setup sqlalchemy models - untested

---
+++ messy/chatroom/models.py
@@ -0,0 +1,30 @@
+from base import Base
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, Text, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+class Chatroom(Base):
+    __tablename__ = 'chatrooms'
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    member_limit = Column(Integer)
+    private = Column(Boolean)
+    password = Column(String)
+    type = Column(String)
+    updated_at = Column(DateTime)
+    created_at = Column(DateTime)
+    deleted_at = Column(DateTime)
+    members = relationship("ChatroomMember")
+    messages = relationship("ChatroomMessage")
+
+
+class ChatroomMessage(Base):
+    __tablename__ = 'chatroom_messages'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer)
+    chatroom_id = Column(Integer, ForeignKey('chatrooms.id'))
+    message = Column(Text)
+    type = Column(String)
+    updated_at = Column(DateTime)
+    created_at = Column(DateTime)
+
---
+++ messy/member/models.py
@@ -0,0 +1,14 @@
+from base import Base
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
+
+
+class ChatroomMember(Base):
+    __tablename__ = 'chatroom_members'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer)
+    chatroom_id = Column(Integer, ForeignKey('chatrooms.id'))
+    notification = Column(Boolean)
+    role = Column(String)
+    updated_at = Column(DateTime)
+    created_at = Column(DateTime)
+

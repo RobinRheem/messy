edea625fa09f0dc78332c3da58816d7349ae1a3e
Robin Rheem <roborheems@gmail.com>
2018-08-01 21:01:41 +0900

    Implement chatrooms API - test - wip

--- messy/chatroom/views.py
+++ messy/chatroom/views.py
@@ -1,5 +1,15 @@
 from sanic import Blueprint
+from sanic.response import json
+from messy.base import scoped_session
+from messy.chatroom.models import Chatroom
 
 
 chatrooms = Blueprint('chatrooms', url_prefix='/api/v1/chatrooms')
+
+
+@chatrooms.get('/')
+async def chatroom_list(request):
+    with scoped_session() as session:
+        response = session.query(Chatroom).first()
+        return json({ 'data': dict(response) if response else None })
 
